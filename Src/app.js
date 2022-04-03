3eb39169103a48d81b44d2ad409daeaf28adeb94
maryamkiann <76832617+maryamkiann@users.noreply.github.com>
2022-04-03 14:15:47 +0430

    last

--- Src/app.js
+++ Src/app.js
@@ -12,7 +12,7 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 let day = days[now.getDay()];
 h4.innerHTML = `${hours}:${minutes} ${day}`;
 
-function formatDya(timestamp) {
+function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -27,20 +27,28 @@ function getForecast(coordinates) {
 
 function displayForecast(response) {
     console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
     let forecastHtml = `<div class="row">`;
 
-    days.forEach(function (forecastDay, index) {
+    forecast.forEach(function (forecastDay, index) {
         if (index < 6) {
-            forecastHtml = forecastHtml + `
+            forecastHtml =
+                forecastHtml +
+                `
         <div class="col-2">
          <div class="weather-forecast-date"> ${formatDay(forecastDay.dt)} </div>
-         <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+         <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon
+                }@2x.png"
             alt="" width="42" />
 
          <div class="temperatures">
-            <span class="weather-forecat-temperature-max"> ${Math.round(forecastDay.temp.max)}°</span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
+            <span class="weather-forecat-temperature-max"> ${Math.round(
+                    forecastDay.temp.max
+                )}°</span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(
+                    forecastDay.temp.min
+                )}°</span>
          </div>
         </div>`;
         }
